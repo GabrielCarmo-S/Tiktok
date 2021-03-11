0b8ff29fcb2ac5639dab3388daaafc60b0146860
Gabriel Carmo <perien.op@gmail.com>
2021-03-11 15:16:03 -0300

    Implement liks of the post

---
+++ data/posts.tsx
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: "p1",
+    videoUri:
+      "https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4",
+    user: {
+      id: "u1",
+      username: "@gabrielcarmo-s",
+      imageUri:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8GrR4JDZhoglUgxri3V6xJFfm60u95UEcA&usqp=CAU",
+    },
+    description: "kkkkkkkkkkkkkkkkkkkkkk",
+    songName: "NF - The Search",
+    songImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8GrR4JDZhoglUgxri3V6xJFfm60u95UEcA&usqp=CAU",
+    likes: 153,
+    comments: 12,
+    shares: 44,
+  },
+
+  {
+    id: 'p2',
+    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u2',
+      username: '@geovana.borges',
+      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8GrR4JDZhoglUgxri3V6xJFfm60u95UEcA&usqp=CAU'
+    },
+    description: 'Hello',
+    songName: 'NF - The Search',
+    songImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8GrR4JDZhoglUgxri3V6xJFfm60u95UEcA&usqp=CAU',
+    likes: 153,
+    comments: 12,
+    shares: 44
+  },
+
+  {
+    id: 'p3',
+    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u3',
+      username: '@cinthia',
+      imageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8GrR4JDZhoglUgxri3V6xJFfm60u95UEcA&usqp=CAU'
+    },
+    description: 'The',
+    songName: 'NF - The Search',
+    songImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu8GrR4JDZhoglUgxri3V6xJFfm60u95UEcA&usqp=CAU',
+    likes: 153,
+    comments: 12,
+    shares: 44
+  }
+];
