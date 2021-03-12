94e4aa4c390751e97095ab266938d9d311d1eca2
Gabriel Carmo <perien.op@gmail.com>
2021-03-12 14:41:17 -0300

    Updates

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { SafeAreaView, StatusBar } from "react-native";
 import Home from "./src/screens/Home";
 import 'react-native-gesture-handler'
 import Navigation from './src/navigation'
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 export default function App() {
   return (
