31aca2a1e1bf17599e758e9ff2f59ae3a58b3c3d
Gabriel Carmo <perien.op@gmail.com>
2021-03-13 13:24:59 -0300

    Updates

--- App.tsx
+++ App.tsx
@@ -1,22 +1,27 @@
 import React, { ReactNode } from "react";
 import { SafeAreaView, StatusBar } from "react-native";
-import 'react-native-gesture-handler'
-import Navigation from './src/navigation'
-import { withAuthenticator } from 'aws-amplify-react-native'
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+import "react-native-gesture-handler";
+import Navigation from "./src/navigation";
+import { withAuthenticator } from "aws-amplify-react-native";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
 
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
 
-const App: () => ReactNode = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="default" />
-      <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+      <SafeAreaView style={{ flex: 1 }}>
         <Navigation />
       </SafeAreaView>
     </>
   );
-}
+};
 
 export default withAuthenticator(App);
