4f0e6f09bc1b0cfa4f26b5f71fc76146914cafd9
Gabriel Carmo <perien.op@gmail.com>
2021-03-13 14:03:30 -0300

    Connect Cognito user with Database user

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { SafeAreaView, StatusBar } from "react-native";
 import "react-native-gesture-handler";
 import Navigation from "./src/navigation";
 import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify from "aws-amplify";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import config from "./aws-exports";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 
 Amplify.configure({
   ...config,
@@ -13,7 +15,49 @@ Amplify.configure({
   },
 });
 
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)];
+};
+
 const App = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (!userInfo) {
+        return;
+      }
+
+      const getUsers = await API.graphql(
+        graphqlOperation(getUser, { id: userInfo.attributes.sub })
+      );
+
+      if (getUsers.data.getUser) {
+        return;
+      }
+
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage(),
+      };
+
+      await API.graphql(graphqlOperation(createUser, { input: newUser }));
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="default" />
